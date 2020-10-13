a49c9d40efd2eed14c424999e9b1d2ceeb9be384
piyush-cosmo <53268607+piyush-cosmo@users.noreply.github.com>
2020-10-13 08:52:51 +0530

    updated user.model.js

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -1,7 +1,28 @@
 import mongoose, { mongo } from 'mongoose'
 
 const UserSchema = new mongoose.Schema({
-
+    name: {
+        type: String,
+        trim: true,
+        required: 'Name is required'
+      },
+      email: {
+        type: String,
+        trim: true,
+        unique: 'Email already exists',
+        match: [/.+\@.+\..+/, 'Please fill a valid email address'],
+        required: 'Email is required'
+      },
+      hashed_password: {
+        type: String,
+        required: "Password is required"
+      },
+      salt: String,
+      updated: Date,
+      created: {
+        type: Date,
+        default: Date.now
+      }
 })
 
 export default mongoose.model('User', UserSchema)
