03dda4a109cca0966a683650e9a828a860afee16
piyush-cosmo <53268607+piyush-cosmo@users.noreply.github.com>
2020-10-14 20:46:59 +0530

    updated user.model.js

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -1,28 +1,28 @@
-import mongoose, { mongo } from 'mongoose'
-
+import mongoose from 'mongoose'
+import crypto from 'crypto'
 const UserSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        trim: true,
-        required: 'Name is required'
-      },
-      email: {
-        type: String,
-        trim: true,
-        unique: 'Email already exists',
-        match: [/.+\@.+\..+/, 'Please fill a valid email address'],
-        required: 'Email is required'
-      },
-      hashed_password: {
-        type: String,
-        required: "Password is required"
-      },
-      salt: String,
-      updated: Date,
-      created: {
-        type: Date,
-        default: Date.now
-      }
+  name: {
+    type: String,
+    trim: true,
+    required: 'Name is required'
+  },
+  email: {
+    type: String,
+    trim: true,
+    unique: 'Email already exists',
+    match: [/.+\@.+\..+/, 'Please fill a valid email address'],
+    required: 'Email is required'
+  },
+  hashed_password: {
+    type: String,
+    required: "Password is required"
+  },
+  salt: String,
+  updated: Date,
+  created: {
+    type: Date,
+    default: Date.now
+  }
 })
 
 UserSchema
@@ -33,27 +33,36 @@ UserSchema
     this.hashed_password = this.encryptPassword(password)
   })
   .get(function() {
-      return this._password
+    return this._password
   })
 
-UserSchema.methods = {
-    authenticate: function(plainText) {
-      return this.encryptPassword(plainText) === this.hashed_password
-    },
-    encryptPassword: function(password) {
-      if (!password) return ''
-      try {
-        return crypto
-          .createHmac('sha1', this.salt)
-          .update(password)
-          .digest('hex')
-      } catch (err) {
-        return ''
-      }
-    },
-    makeSalt: function() {
-      return Math.round((new Date().valueOf() * Math.random())) + ''
-    }
+UserSchema.path('hashed_password').validate(function(v) {
+  if (this._password && this._password.length < 6) {
+    this.invalidate('password', 'Password must be at least 6 characters.')
   }
+  if (this.isNew && !this._password) {
+    this.invalidate('password', 'Password is required')
+  }
+}, null)
+
+UserSchema.methods = {
+  authenticate: function(plainText) {
+    return this.encryptPassword(plainText) === this.hashed_password
+  },
+  encryptPassword: function(password) {
+    if (!password) return ''
+    try {
+      return crypto
+        .createHmac('sha1', this.salt)
+        .update(password)
+        .digest('hex')
+    } catch (err) {
+      return ''
+    }
+  },
+  makeSalt: function() {
+    return Math.round((new Date().valueOf() * Math.random())) + ''
+  }
+}
 
 export default mongoose.model('User', UserSchema)
