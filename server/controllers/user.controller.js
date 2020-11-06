4635b07e48702db372946243d2700f8968d95ccc
piyush-cosmo <53268607+piyush-cosmo@users.noreply.github.com>
2020-11-07 01:34:38 +0530

    updating user.controller.js

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -2,3 +2,19 @@ import User from '../models/user.model'
 import extend from 'lodash/extend'
 import errorHandler from './../helpers/dbErrorHandler'
 
+//Creating a new user
+const create = async (req, res) => {
+    const user = new User(req.body)
+    try {
+      await user.save()
+      return res.status(200).json({
+        message: "Successfully signed up!"
+      })
+    } catch (err) {
+      return res.status(400).json({
+        error: errorHandler.getErrorMessage(err)
+      })
+    }
+  }
+
+export default create
