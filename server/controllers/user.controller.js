7179024f7b7b897f499ada854346e6c2e87ccb0f
piyush-cosmo <53268607+piyush-cosmo@users.noreply.github.com>
2020-11-07 19:28:11 +0530

    updating user.controller.js

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -17,4 +17,16 @@ const create = async (req, res) => {
     }
   }
 
+
+const list = async (req, res) => {
+    try {
+      let users = await User.find().select('name email updated created')
+      res.json(users)
+    } catch (err) {
+      return res.status(400).json({
+        error: errorHandler.getErrorMessage(err)
+      })
+    }
+  }
+  
 export default create
