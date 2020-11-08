cb7310a79f5a5873b41470f5e43c15a71386010f
piyush-cosmo <53268607+piyush-cosmo@users.noreply.github.com>
2020-11-09 00:27:20 +0530

    updated express.js

--- server/express.js
+++ server/express.js
@@ -5,7 +5,9 @@ import compress from 'compression'
 import cors from 'cors'
 import helmet from 'helmet'
 
-import Template from './../template'
+import userRoutes from './routes/user.routes'
+
+// import Template from './../template'
 
 const app = express()
 
@@ -14,14 +16,19 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(cookieParser())
 app.use(compress())
+
 // secure apps by setting various HTTP headers
 app.use(helmet())
+
 // enable CORS - Cross Origin Resource Sharing
 app.use(cors())
 
-//request-respone fo root URL "/"
-app.get('/', (req, res) => {
-    res.status(200).send(Template())
-})
+// request-respone for root URL for rendering template.js in browser "/"
+// app.get('/', (req, res) => {
+//     res.status(200).send(Template())
+// })
+
+// mount routes
+app.use('/', userRoutes)
 
 export default app
