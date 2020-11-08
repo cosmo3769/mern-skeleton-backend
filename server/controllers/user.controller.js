0782c64e61c59ab5009d89e8e1e0cf2e0aee97b4
piyush-cosmo <53268607+piyush-cosmo@users.noreply.github.com>
2020-11-08 18:49:15 +0530

    updating user.controller.js

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -28,6 +28,23 @@ const list = async (req, res) => {
       })
     }
   }
+ 
+//Load the user and append to req object  
+const userByID = async (req, res, next, id) => {
+    try {
+      let user = await User.findById(id)
+      if (!user)
+        return res.status('400').json({
+          error: "User not found"
+        })
+      req.profile = user
+      next()
+    } catch (err) {
+      return res.status('400').json({
+        error: "Could not retrieve user"
+      })
+    }
+  }
   
 export default {
     create,
