56e1a83c0b211917882f0837da26063ebff5d976
piyush-cosmo <53268607+piyush-cosmo@users.noreply.github.com>
2020-11-08 21:30:40 +0530

    updating user.controller.js

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -68,7 +68,22 @@ const update = async (req, res) => {
         error: errorHandler.getErrorMessage(err)
       })
     }
-  }  
+  }
+  
+//Deleting the single user by userId
+const remove = async (req, res) => {
+    try {
+      let user = req.profile
+      let deletedUser = await user.remove()
+      deletedUser.hashed_password = undefined
+      deletedUser.salt = undefined
+      res.json(deletedUser)
+    } catch (err) {
+      return res.status(400).json({
+        error: errorHandler.getErrorMessage(err)
+      })
+    }
+  }
 
 export default {
     create,
