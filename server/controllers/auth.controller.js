99e9966d977e165997008dc02b456d5748814bd4
piyush-cosmo <53268607+piyush-cosmo@users.noreply.github.com>
2020-11-10 21:57:34 +0530

    updating auth.controller.js

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -2,3 +2,48 @@ import User from '../models/user.model'
 import jwt from 'jsonwebtoken'
 import expressJwt from 'express-jwt'
 import config from './../../config/config'
+
+//signin controller function
+const signin = async (req, res) => {
+    try {
+      let user = await User.findOne({
+        "email": req.body.email
+      })
+      if (!user)
+        return res.status('401').json({
+          error: "User not found"
+        })
+  
+      if (!user.authenticate(req.body.password)) {
+        return res.status('401').send({
+          error: "Email and password don't match."
+        })
+      }
+  
+      const token = jwt.sign({
+        _id: user._id
+      }, config.jwtSecret)
+  
+      res.cookie("t", token, {
+        expire: new Date() + 9999
+      })
+  
+      return res.json({
+        token,
+        user: {
+          _id: user._id,
+          name: user.name,
+          email: user.email
+        }
+      })
+  
+    } catch (err) {
+  
+      return res.status('401').json({
+        error: "Could not sign in"
+      })
+  
+    }
+  }
+
+export default signin
