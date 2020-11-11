d81e244f686aa74c9ba78caf5ba93d883efc9857
piyush-cosmo <53268607+piyush-cosmo@users.noreply.github.com>
2020-11-12 00:15:27 +0530

    updating auth.controller.js

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -60,6 +60,17 @@ const requireSignin = expressJwt({
   userProperty: 'auth'
 })
 
+//authorization for authenticated user to update or delete 
+const hasAuthorization = (req, res, next) => {
+  const authorized = req.profile && req.auth && req.profile._id == req.auth._id
+  if (!(authorized)) {
+    return res.status('403').json({
+      error: "User is not authorized"
+    })
+  }
+  next()
+}
+
 export default {
     signin,
     signout,
